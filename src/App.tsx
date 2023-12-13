9fe573fc681b6be6627ec67e60fbdc78ec07c7d9
Jeremy FRAZIER <frazierjeremi@gmail.com>
2023-12-13 09:43:20 +0100

    handelclick

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const router = createBrowserRouter([
     element: <Connexion />,
   },
   {
-    path: '/',
+    path: '/home',
     element: <Home />,
   }
 ])
--- src/routes/Connexion.tsx
+++ src/routes/Connexion.tsx
@@ -1,8 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function Connexion(){
     const [password, setPassword] = useState('')
     const [email, setEmail] = useState('')
+
+    const handelclick =  async () => {
+        console.log('form', email, password)    
+        const response = await fetch("http://localhost:1337/api/auth/local", {
+            headers: new Headers({
+              "Content-Type": "application/json"
+            }),
+            method: "POST",
+            body: JSON.stringify({
+                "identifier": email,
+                "password": password
+              }),
+          })
+          console.log(response)
+          const data = await response.json()
+          console.log('response auth',data)
+    }
+
+    
 
     return (
         <div>
@@ -12,7 +32,7 @@ export default function Connexion(){
                 <input type="email" id="email" name="email" onChange={(e) => setEmail(e.target.value)} />
                 <label htmlFor="password">Password</label>
                 <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} />
-                <button>Se connecter</button>
+                <button onClick={handelclick}>Se connecter</button>
             </div>
         </div>
     )
